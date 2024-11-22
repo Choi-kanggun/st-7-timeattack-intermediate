6738dec8ed6e27d0a699f3c765fd196294982e25
Choi-kanggun <kg4889@gmail.com>
2024-11-22 16:34:38 +0900

    calculator

--- src/redux/slices/calculatorSlice.js
+++ src/redux/slices/calculatorSlice.js
@@ -9,15 +9,15 @@ export const counterSlice = createSlice({
     value: 0,
   },
   reducers: {
-    increment: (state) => {
+    increment: (state, action) => {
       // Redux Toolkit allows us to write "mutating" logic in reducers. It
       // doesn't actually mutate the state because it uses the Immer library,
       // which detects changes to a "draft state" and produces a brand new
       // immutable state based off those changes
-      state.value += 1;
+      state.value += Number(action.payload);
     },
-    decrement: (state) => {
-      state.value -= 1;
+    decrement: (state, action) => {
+      state.value -= Number(action.payload);
     },
     incrementByAmount: (state, action) => {
       state.value += action.payload;
